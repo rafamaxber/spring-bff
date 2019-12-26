aef86dce828299e965f13780aff726d90acdbd57
Rafael Bernardo Dos Santos Silva <rafael.rbernardo@mercadolivre.com>
2019-12-26 20:06:12 -0300

    add new method list all categories

--- src/main/java/com/bff/resources/CategoriaResource.java
+++ src/main/java/com/bff/resources/CategoriaResource.java
@@ -10,12 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bff.domain.Categoria;
 import com.bff.services.CategoriaService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<Categoria> listar() {
+//		Categoria cat1 = new Categoria(1, "Teste");
+//		Categoria cat2 = new Categoria(2, "Teste2");
+//
+//		List<Categoria> lista = new ArrayList<>();
+//		lista.add(cat1);
+//		lista.add(cat2);
+
+
+		return service.listar();
+	}
 	
 	@RequestMapping( value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
--- src/main/java/com/bff/services/CategoriaService.java
+++ src/main/java/com/bff/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.bff.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,15 @@ public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
+
+	public List<Categoria> listar() {
+
+		return new ArrayList<>(repo.findAll());
+	}
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
 	}
+
 }
